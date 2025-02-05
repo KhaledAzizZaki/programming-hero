a92241f4f978297477b4a09d71cf4b5f8eca701e
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-05 13:07:57 +0600

    practice

---
+++ module17/practice.js
@@ -0,0 +1,25 @@
+/*****practice task1******/
+
+let moneyIHave = 1000;
+
+let orange = 400;
+let apple = 300;
+
+let totalPrice = orange + apple;
+
+let shopkeeperReturn = moneyIHave - totalPrice;
+
+console.log("Shopkeeper Return", shopkeeperReturn, "taka");
+
+/*****practice task2******/
+
+let mathematics = 67.56;
+let biology = 35;
+let chemistry = 50;
+let physics = 70;
+let bangla = 45;
+
+let totalMarks = mathematics + biology + chemistry + physics + bangla;
+
+let averageNumber = totalMarks / 5;
+console.log("Average Number", averageNumber.toFixed(2));
