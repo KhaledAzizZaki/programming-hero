1098a9014d7912485069df766a15c48a89d4c116
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-06 18:45:05 +0600

    switch

--- module17/practice2-2.js
+++ module17/practice2-2.js
@@ -55,3 +55,23 @@ if (score >= 80) {
 } else {
   console.log("Go home and sleep and act sad");
 }
+
+/****** example *******/
+let theme = "blue";
+
+switch (theme) {
+  case "light": {
+    console.log("light theme applied");
+    break;
+  }
+  case "dark": {
+    console.log("dark theme applied");
+    break;
+  }
+  case "blue": {
+    console.log("blue theme applied");
+    break;
+  }
+  default:
+    console.log("System theme applied");
+}
