468e030cb02bce31c1b813a97b4958e24c775256
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-07 21:25:40 +0600

    practice

---
+++ module17/practice-from-youtub.js
@@ -0,0 +1,59 @@
+/****** For loop *********/
+// for (let i = 1; i <= 100; i++) {
+//   console.log("Khaled Aziz Zaki");
+// }
+/****** For loop *********/
+// let sum = 0;
+
+// for (let i = 1; i <= 5; i++) {
+//   sum += i;
+// }
+// console.log(sum);
+/****** While loop *********/
+// let i = 1;
+
+// while (i <= 5) {
+//   console.log("i =", i);
+//   i++;
+// }
+/****** While loop *********/
+// let sum = 0;
+// let i = 1;
+
+// while (i <= 10) {
+//   sum += i;
+//   i++;
+// }
+
+// console.log("sum =", sum);
+
+/****** Do While loop *********/
+// let i = 1;
+
+// do {
+//   console.log("i =", i);
+//   i++;
+// } while (i <= 5);
+
+/****** Do While loop *********/
+// let sum = 0;
+// let i = 10;
+
+// do {
+//   sum += i;
+//   i++;
+// } while (i <= 5);
+
+// console.log("sum =", sum);
+
+/****** practice *********/
+
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
