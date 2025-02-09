1b9d362c2a5c14ffcbed687441dee338040efdff
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-09 18:10:39 +0600

    for loop

---
+++ module17/for-loop-tasks.js
@@ -0,0 +1,106 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+/*programming hero*/
+
+// for (let i = 1; i <= 60; i++) {
+//   console.log(i, "I will invest at least 6 hrs every single day for next day.");
+// }
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+// for (let i = 61; i <= 100; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// for (let i = 61; i <= 100; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 91 to 129.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 51 to 85.
+
+ */
+
+/*programming hero*/
+
+// let sum = 0;
+
+// for (let i = 91; i <= 129; i++) {
+//   if (i % 2 == 1) {
+//     sum = sum + i;
+//   }
+// }
+
+// console.log(sum);
+
+// let sum = 0;
+
+// for (let i = 91; i <= 129; i++) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+// }
+
+// console.log(sum);
+
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+/*programming hero*/
+
+// let num = 9;
+// let multi = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//   multi = num * i;
+//   console.log(num, "X", i, "=", multi);
+// }
+
+/***
+
+Implement a countdown timer that counts down from 81 to 65.
+
+ */
+
+/*programming hero*/
+
+for (let i = 81; i >= 65; i--) {
+  console.log(i);
+}
