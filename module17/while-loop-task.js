d5f1ec9a77091ea965119b7a9f189973a8541d4d
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-09 18:47:57 +0600

    while loop

--- module17/while-loop-task.js
+++ module17/while-loop-task.js
@@ -5,3 +5,121 @@
  */
 
 /*programming hero*/
+// let i = 1;
+
+// while (i <= 60) {
+//   console.log(
+//     i,
+//     "I will invest at least 6 hrs every single day for next 60 days!"
+//   );
+//   i++;
+// }
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+// let i = 61;
+
+// while (i <= 100) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+// let i = 61;
+
+// while (i <= 100) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+//   i++;
+// }
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+/*programming hero*/
+
+// let i = 81;
+// let sum = 0;
+
+// while (i <= 131) {
+//   if (i % 2 == 1) {
+//     sum = sum + i;
+//   }
+//   i++;
+// }
+
+// console.log(sum);
+
+// let i = 206;
+// let sum = 0;
+
+// while (i <= 311) {
+//   if (i % 2 == 0) {
+//     sum = sum + i;
+//   }
+//   i++;
+// }
+
+// console.log(sum);
+
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+/*programming hero*/
+
+// let num = 5;
+// // let multi = 0;
+// let i = 1;
+
+// while (i <= 10) {
+//   const multi = num * i;
+//   console.log(num, "X", i, "=", multi);
+//   i++;
+// }
+
+/***
+
+Implement a countdown timer that counts down from 21 to 15.
+
+ */
+
+/*programming hero*/
+
+let i = 21;
+while (i >= 15) {
+  console.log(i);
+  i--;
+}
