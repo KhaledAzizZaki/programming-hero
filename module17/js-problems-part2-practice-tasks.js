8f6967ff1841ecd5f56b53bbe640ce9254ab70d3
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-15 15:59:33 +0600

    practice part 2

--- module17/js-problems-part2-practice-tasks.js
+++ module17/js-problems-part2-practice-tasks.js
@@ -1,7 +1,6 @@
 // Task -1:
 // Find the lowest number in the array below.
 
-/*
 const heights2 = [167, 190, 120, 165, 137];
 let lowest = heights2[0];
 
@@ -12,24 +11,21 @@ for (let i of heights2) {
 }
 
 console.log(lowest);
-*/
 
 // Task -2:
 // Find the friend with the smallest name.
 
-/*
-const heights2 = ["rahim", "robin", "rafi", "ron", "rashed"];
+const heights3 = ["rahim", "robin", "rafi", "ron", "rashed"];
 
-let shortName = heights2[0];
+let shortName = heights3[0];
 
-for (let i of heights2) {
+for (let i of heights3) {
   if (i.length < shortName.length) {
     shortName = i;
   }
 }
 
 console.log(shortName);
-*/
 
 // Task-3:
 // Your task is to calculate the total budget required to buy electronics:
@@ -39,7 +35,6 @@ console.log(shortName);
 //     mobile = 20000 tk
 // Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
 
-/*
 function totalMoney(a, b, c) {
   let laptop = 35000;
   let tablet = 15000;
@@ -56,7 +51,6 @@ function totalMoney(a, b, c) {
 totalMoney(1, 1, 1);
 totalMoney(2, 4, 5);
 totalMoney(6, 1, 2);
-*/
 
 // Task-4:
 // You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
@@ -78,7 +72,29 @@ function average(a) {
     total = total + i.price;
   }
   let avg = total / a.length;
-  console.log(Math.round(avg));
+  console.log("Average Price:", Math.round(avg));
 }
 
 average(phones);
+
+// Task -5: (Hard)
+// For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+
+const employees = [
+  { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+  { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+  { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+  { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+function totalSalary(a) {
+  let totalSalaries = 0;
+  for (let i of a) {
+    let totalIncrement = i.experience * i.increment;
+    let cSalary = totalIncrement + i.starting;
+    totalSalaries += cSalary;
+  }
+  console.log("Total Salaries:", totalSalaries);
+}
+
+totalSalary(employees);
