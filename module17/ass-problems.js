5a9da13c7bab30f8e298de8fead5567ec9b69f15
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-17 14:33:49 +0600

    assignment problem2 solve

--- module17/ass-problems.js
+++ module17/ass-problems.js
@@ -1,11 +1,37 @@
-function calculateTex(a, b) {
-  if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0 || a < b) {
-    return "invalid input";
+/*
+ ******Tex Calculate*********
+ */
+
+// function calculateTex(a, b) {
+//   if (
+//     typeof a !== "number" ||
+//     typeof b !== "number" ||
+//     a < 0 ||
+//     b < 0 ||
+//     a < b
+//   ) {
+//     return "invalid input";
+//   }
+//   let profit = a - b;
+//   let tex = (profit * 20) / 100;
+//   return tex;
+// }
+
+// let result = calculateTex(5000, 1500);
+// console.log(result);
+
+/*
+ **********Send Notification***********
+ */
+
+function sendNotification(a) {
+  if (typeof a !== "string" || a == false || !a.includes("@")) {
+    return "Invalid Input";
   }
-  let profit = a - b;
-  let tex = (profit * 20) / 100;
-  return tex;
+  let str = a.split("@");
+  let notification = `${str[0]} send You an email from ${str[1]}`;
+  return notification;
 }
 
-let result = calculateTex(10000, 3000);
-console.log(result);
+let result1 = sendNotification("khaledazizzaki@gmail.com");
+console.log(result1);
