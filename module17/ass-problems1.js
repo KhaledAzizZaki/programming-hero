510515d67d045ee33cd7fb18dbc2bb452284855f
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-18 14:31:18 +0600

    ass problem-02

--- module17/ass-problems1.js
+++ module17/ass-problems1.js
@@ -8,3 +8,28 @@ function calculateVAT(price) {
 
   return vat;
 }
+// Problem-02
+function validContact(contact) {
+  if (typeof contact != "string") {
+    return "Invalid";
+  }
+
+  let num = true;
+
+  for (let i of contact) {
+    if (isNaN(i)) {
+      num = false;
+      break;
+    }
+  }
+
+  if (
+    num &&
+    contact.length == 11 &&
+    !contact.includes(" ") &&
+    contact[0] == 0 &&
+    contact[1] == 1
+  ) {
+    return true;
+  } else return false;
+}
