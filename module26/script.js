97580c8ede1699cc49f883848d15a55bcdc201e2
Khaled Aziz Zaki <khaledazizzaki@gmail.com>
2025-02-24 16:23:12 +0600

    event listener

--- module26/script.js
+++ module26/script.js
@@ -1,15 +1,27 @@
 function green() {
   document.body.style.backgroundColor = "limegreen";
 }
+
 function red() {
   document.body.style.backgroundColor = "red";
 }
+
 let makeBlue = document.getElementById("make-blue");
 makeBlue.onclick = function blue() {
   document.body.style.backgroundColor = "blue";
 };
+
 const makePurple = document.getElementById("make-purple");
 makePurple.onclick = purple;
 function purple() {
   document.body.style.backgroundColor = "darkgray";
 }
+
+document
+  .getElementById("make-black")
+  .addEventListener("click", function black() {
+    document.body.style.backgroundColor = "black";
+  });
+document.getElementById("make-gold").addEventListener("click", function gold() {
+  document.body.style.backgroundColor = "goldenrod";
+});
